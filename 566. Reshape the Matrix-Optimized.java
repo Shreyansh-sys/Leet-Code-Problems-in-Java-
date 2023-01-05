099375cacc249af983e1023082ad823fc64f23f6
Shreyansh-sys <65069645+Shreyansh-sys@users.noreply.github.com>
2023-01-05 06:33:27 +0530

    Add files via upload

---
+++ 566. Reshape the Matrix-Optimized.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int rows = mat.length;
+        int cols = mat[0].length;
+
+        if((rows*cols)!=(r*c)) return mat;
+
+        int[][] res = new int [r][c];
+        
+        for(int i=0; i<r*c; i++){
+            res[i/c][i%c] = mat[i/cols][i%cols];
+        }
+        return res;
+    }
+}
