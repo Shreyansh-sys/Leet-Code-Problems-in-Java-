d7bab03f96bcc329f6d6e966b3ea062bedb7a6c3
Shreyansh-sys <65069645+Shreyansh-sys@users.noreply.github.com>
2023-03-09 05:29:57 +0530

    Add files via upload

---
+++ 209. Minimum Size Subarray Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int windowStart = 0; int minLength = Integer.MAX_VALUE;
+        int windowSum = 0;
+        for(int windowEnd = 0; windowEnd < nums.length; windowEnd++){
+            windowSum += nums[windowEnd];
+            while(windowSum >= target){
+                minLength = Math.min(minLength, windowEnd - windowStart + 1);
+                windowSum -= nums[windowStart];
+                windowStart++;
+            }
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+}
