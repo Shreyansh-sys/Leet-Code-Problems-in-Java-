f12845a7288a9ef094bef40a5644974f7b7e9e2c
Shreyansh-sys <65069645+Shreyansh-sys@users.noreply.github.com>
2023-05-19 12:28:03 -0500

    Add files via upload

---
+++ 143. Reorder List.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null)
+        return;
+
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode headSecondHalf = reverse(slow);
+        ListNode headFirstHalf = head;
+
+        while(headFirstHalf != null && headSecondHalf != null){
+            ListNode temp = headFirstHalf.next;
+            headFirstHalf.next = headSecondHalf;
+            headFirstHalf = temp;
+
+            temp = headSecondHalf.next;
+            headSecondHalf.next = headFirstHalf;
+            headSecondHalf = temp;
+        }
+
+        if(headFirstHalf != null)
+        headFirstHalf.next = null;
+    }
+
+    private static ListNode reverse(ListNode head){
+        ListNode prev = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
