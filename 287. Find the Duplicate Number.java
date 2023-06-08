37d14bb5faf7d497e3c35e1fafa3bd1fa7eafca9
Shreyansh-sys <65069645+Shreyansh-sys@users.noreply.github.com>
2023-06-08 12:20:16 -0500

    Add files via upload

---
+++ 287. Find the Duplicate Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int [] count = new int [nums.length];
+        for(int i = 0; i < nums.length; i++){
+            count[nums[i]];
+            if(count[nums[i]] > 1)
+            return nums[i];
+        }
+        return -1;
+    }
+}
